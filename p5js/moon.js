184dcccc8ee3b7a81f27a32965fa5ad53e7a8f6b
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2020-11-29 14:40:16 +0530

    Create moon.js

---
+++ p5js/moon.js
@@ -0,0 +1,36 @@
+function setup(){
+  
+  createCanvas(650,350);
+  
+}
+
+function draw() {
+  
+  background(0, 0, 26);
+  noStroke();
+  
+  fill(255, 255, 204);
+  ellipse(50,150,60);
+  ellipse(150,150,60)
+  ellipse(250,150,60);
+  ellipse(350,150,60);
+  ellipse(450,150,60);
+  ellipse(550,150,60);
+  ellipse(550,260,60);
+  
+  noStroke();
+  
+  fill(0, 0, 26);
+  ellipse(25,114,30);
+  ellipse(120,150,30);
+  ellipse(220,150,40);
+  ellipse(320,150,50);
+  ellipse(420,150,70);
+  ellipse(520,140,85);
+  ellipse(520,250,105);
+  
+  strokeWeight(0.5);
+  stroke(255, 255, 204);
+  ellipse(450,260,60);
+  
+}
